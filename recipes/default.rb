de1e2309dc7dee692d677cd8ba0437ca861e675f
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-17 14:48:44 +0100

    Test's are passing

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+
+
+apt_update 'update_sources' do
+  action :update
+end
+
+
+
+package 'packer'
+
+package 'chef'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,15 +15,12 @@ describe 'ChefPackerAMICookbook::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
+    it 'installs packer' do
+      expect(chef_run).to install_package('packer')
+    end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs chef' do
+      expect(chef_run).to install_package('chef')
     end
   end
 end
